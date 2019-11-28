f679fbfc41632c057eafc938072a44f0deba5e06
Youssef1998d <youssefdrira32@gmail.com>
2019-11-28 09:26:04 +0100

    le fichier verification terminé v01

---
+++ verification.py
@@ -0,0 +1,24 @@
+def verifCol(liste):
+    for i in range(0,6,3):
+        if "".join(liste[i:i+3])=="XXX" or "".join(liste[i:i+3])=="OOO":
+            return True
+    return False
+
+def verifRow(liste):
+    for i in range(3):
+        if liste[i]+liste[i+3]+liste[i+6] == "XXX" or l[i]+l[i+3]+l[i+6] == "OOO":
+            return True
+    return False
+
+def verifDiag(liste):
+    if liste[0]+liste[4]+liste[8] == "OOO" or liste[0]+liste[4]+liste[8] == "XXX":
+        return True
+    elif liste[2]+liste[4]+liste[6] == "XXX" or liste[2]+liste[4]+liste[6] == "OOO":
+        return True
+    return False
+
+def verifAll(liste):
+    return verifCol(liste) or verifDiag(liste) or verifRow(liste)
+[0, 1, 2,
+ 3, 4, 5,
+ 6, 7, 8]
